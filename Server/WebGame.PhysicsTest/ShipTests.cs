7492dcdcdf640ba82ca2febb21c7156a1632534b
McKay Salisbury (Kanei) <mckaysalisbury@hotmail.com>
2013-01-25 21:57:04 -0700

    Updating for turn radius

---
+++ Server/WebGame.PhysicsTest/ShipTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebGame.PhysicsTest
+{
+    [TestClass]
+    public class ShipTests
+    {
+        [TestMethod]
+        public void RotationBigUpdate()
+        {
+            var ship = new Ship();
+            var initialOrientation = ship.Orientation;
+            var desiredOrientation = initialOrientation + Math.PI / 4;
+            ship.DesiredOrientation = desiredOrientation;
+            ship.Update(TimeSpan.MaxValue);
+            Assert.AreEqual(desiredOrientation, ship.Orientation, "Orientation didn't match");
+        }
+
+        [TestMethod]
+        public void RotationBigManySmallUpdates()
+        {
+            var ship = new Ship();
+            var initialOrientation = ship.Orientation;
+            var desiredOrientation = initialOrientation + Math.PI / 4;
+            ship.DesiredOrientation = desiredOrientation;
+            for (int i = 0; i < 10000; i++)
+            {
+                ship.Update(TimeSpan.FromMilliseconds(1));
+            }
+            Assert.AreEqual(desiredOrientation, ship.Orientation, "Orientation didn't match");
+        }
+
+        [TestMethod]
+        public void RotateNotWholeAmount()
+        {
+            var ship = new Ship();
+            var initialOrientation = ship.Orientation;
+            var desiredOrientation = initialOrientation + Math.PI / 4;
+            ship.DesiredOrientation = desiredOrientation;
+            var oldDiffMag = Math.Abs(ship.Orientation - desiredOrientation);
+            ship.Update(TimeSpan.FromMilliseconds(1));
+            var newDiffMag = Math.Abs(ship.Orientation - desiredOrientation);
+            Assert.AreNotEqual(initialOrientation, ship.Orientation, "The orientation hasn't changed.");
+            Assert.IsTrue(newDiffMag < oldDiffMag, "We didn't get closer.");
+
+        }
+    }
+}
