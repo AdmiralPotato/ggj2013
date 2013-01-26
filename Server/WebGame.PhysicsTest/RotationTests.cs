662dff1c08c7cbf75ab8e11dea900297d1482f99
McKay Salisbury (Kanei) <mckaysalisbury@hotmail.com>
2013-01-26 00:04:23 -0700

    add tests

---
+++ Server/WebGame.PhysicsTest/RotationTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebGame.PhysicsTest
+{
+    [TestClass]
+    public class RotationTests
+    {
+        [TestMethod]
+        public void RotationBigUpdate()
+        {
+            var ship = new Ship();
+            var initialOrientation = ship.Orientation;
+            var desiredOrientation = initialOrientation + Math.PI / 4;
+            ship.DesiredOrientation = desiredOrientation;
+            ship.Update(TimeSpan.MaxValue);
+            Assert.AreEqual(desiredOrientation, ship.Orientation, "Orientation didn't match");
+        }
+
+        [TestMethod]
+        public void RotationBigManySmallUpdates()
+        {
+            var ship = new Ship();
+            var initialOrientation = ship.Orientation;
+            var desiredOrientation = initialOrientation + Math.PI / 4;
+            ship.DesiredOrientation = desiredOrientation;
+            for (int i = 0; i < 10000; i++)
+            {
+                ship.Update(TimeSpan.FromMilliseconds(1));
+            }
+            Assert.AreEqual(desiredOrientation, ship.Orientation, "Orientation didn't match");
+        }
+
+        [TestMethod]
+        public void RotateNotWholeAmount()
+        {
+            var ship = new Ship();
+            var initialOrientation = ship.Orientation;
+            var desiredOrientation = initialOrientation + Math.PI / 4;
+            ship.DesiredOrientation = desiredOrientation;
+            var oldDiffMag = Math.Abs(ship.Orientation - desiredOrientation);
+            ship.Update(TimeSpan.FromMilliseconds(1));
+            var newDiffMag = Math.Abs(ship.Orientation - desiredOrientation);
+            Assert.AreNotEqual(initialOrientation, ship.Orientation, "The orientation hasn't changed.");
+            Assert.IsTrue(newDiffMag < oldDiffMag, "We didn't get closer.");
+        }
+
+        [TestMethod]
+        public void ShouldNotRotateBackwards()
+        {
+            var ship = new Ship();
+            ship.DesiredOrientation = 1;
+            for (int i = 0; i < 1000; i++)
+            {
+                ship.Update(TimeSpan.FromSeconds(0.25));
+                Assert.IsFalse(ship.Orientation < 0);
+            }
+        }
+    }
+}
---
+++ Server/WebGame.PhysicsTest/VelocityTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebGame.PhysicsTest
+{
+    [TestClass]
+    public class VelocityTests
+    {
+        [TestMethod]
+        public void IncreaseVelocity()
+        {
+            var ship = new Ship();
+        }
+    }
+}
