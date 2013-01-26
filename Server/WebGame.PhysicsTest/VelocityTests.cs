3a69b0d4f82841f4f5ef8f507061cafaad68433d
McKay Salisbury (Kanei) <mckaysalisbury@hotmail.com>
2013-01-26 00:50:02 -0700

    Adding velocity code

--- Server/WebGame.PhysicsTest/VelocityTests.cs
+++ Server/WebGame.PhysicsTest/VelocityTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
 
 namespace WebGame.PhysicsTest
 {
@@ -10,6 +11,11 @@ namespace WebGame.PhysicsTest
         public void IncreaseVelocity()
         {
             var ship = new Ship();
+            ship.ImpulsePercentage = 100;
+            Assert.AreEqual(ship.VelocityMetersPerSecond, Vector3.Zero, "Ship wasn't created at rest");
+            ship.Update(TimeSpan.FromSeconds(0.25));
+            Assert.AreNotEqual(ship.VelocityMetersPerSecond, Vector3.Zero, "Ship isn't moving.");
+            Assert.AreNotEqual(ship.Position, Vector3.Zero, "Ship hasn't moved.");
         }
     }
 }
