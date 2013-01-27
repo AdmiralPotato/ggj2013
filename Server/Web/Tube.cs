e1d29ff515c54f38674712788910699f0458d9a4
McKay Salisbury (Kanei) <mckaysalisbury@hotmail.com>
2013-01-27 01:30:03 -0700

    Add missing file

---
+++ Server/Web/Tube.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGame
+{
+    public class Tube
+    {
+        public TimeSpan ProjectileLoadTime { get; set; }
+        public ProjectileStatus ProjectileStatus { get; set; }
+    }
+}
